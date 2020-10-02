945bdf4019cbc0586d5febe211a3842da9b82858
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-10-02 10:04:46 +0200

    Add project files.

---
+++ Obligatirisk Opgave 1/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Obligatirisk_Opgave_1
+{
+    public class FanOut
+    {
+        public int Id { get; set; }
+        private string _name;
+        private double _temp;
+        private double _fugt;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Not Valid");
+                }
+            }
+        }
+
+        public double Temp
+        {
+            get { return _temp; }
+            set
+            {
+                if (value < 15)
+                {
+                    throw new System.ArgumentException("Temp er for lav");
+                }
+                else if (value > 25)
+                {
+                    throw new System.ArgumentException("Temp er for høj");
+                }
+                else
+                {
+                    _temp = value;
+                }
+            }
+        }
+
+        public double Fugt
+        {
+            get { return _fugt; }
+            set
+            {
+                if (value < 30)
+                {
+                    throw new System.ArgumentException("For lidt fugt");
+                }
+                else if (value > 80)
+                {
+                    throw new System.ArgumentException("For meget fugt");
+                }
+                else
+                {
+                    _fugt = value;
+                }
+            }
+        }
+
+        public FanOut()
+        {
+
+        }
+
+    }
+}
+
